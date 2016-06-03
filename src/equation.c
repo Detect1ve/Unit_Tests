6055fb801792225aaecbcd7b9fada311e00c116d
Detect1ve <detect1ve@ubuntu.ubuntu-domain>
2016-06-04 02:57:30 +0800

    import equation.c

---
+++ src/equation.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <math.h>
+
+int roots (float a, float b, float c, float *x1, float *x2)
+{
+	float d;
+
+	d = b * b - 4 * a * c;
+        if (d == 0)
+	{
+		*x1 = *x2 = - b / (2 * a);
+                return 1;
+	}
+	else
+	{
+		*x1 = (- b + sqrt(d)) / (2 * a);
+		*x2 = (- b - sqrt(d)) / (2 * a);
+		return 2;
+	}
+	return 0;
+}
