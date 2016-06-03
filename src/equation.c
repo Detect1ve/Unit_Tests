dd5c6ba609408b1d83be0f7a6d3a1341bcb25f71
Detect1ve <detect1ve@ubuntu.ubuntu-domain>
2016-06-04 06:16:14 +0800

    check negative discriminant

--- src/equation.c
+++ src/equation.c
@@ -21,6 +21,8 @@ int roots (float a, float b, float c, float *x1, float *x2)
 		return 1;
 	}
 	d = b * b - 4 * a * c;
+	if (d < 0)
+		return 0;
         if (d == 0)
 	{
 		*x1 = *x2 = - b / (2 * a);
