60667eca943fc4bce03fc7b5a29e5325a148670e
Detect1ve <detect1ve@ubuntu.ubuntu-domain>
2016-06-04 03:48:18 +0800

    Create first test

--- test/equation_test.c
+++ test/equation_test.c
@@ -1,4 +1,21 @@
 #include "ctest.h"
 #include "equation.h"
 
-
+CTEST(distance_suite, simple_test) {
+	//Given
+	float a = 1;
+	float b = -2;
+	float c = -3;
+	
+	//When
+	float x1, x2;
+	int x = roots(a, b, c, &x1, &x2);
+	
+	//Then
+	float expected_x1 = 3;
+	float expected_x2 = -1;
+	
+	ASSERT_EQUAL(x, 2);
+	ASSERT_EQUAL(expected_x1, x1);
+	ASSERT_EQUAL(expected_x2, x2);
+}
