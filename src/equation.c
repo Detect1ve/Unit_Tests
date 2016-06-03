2d70f6d5163589e0087b106910dc8fddcefd44c0
Detect1ve <detect1ve@ubuntu.ubuntu-domain>
2016-06-04 03:58:52 +0800

    The latest addition inspection for value

--- src/equation.c
+++ src/equation.c
@@ -10,6 +10,11 @@ int roots (float a, float b, float c, float *x1, float *x2)
 		return 0;
 	if (a == 0 && b == 0)
 		return 0;
+	if (a == 0 && c == 0)
+	 {
+		*x1 = *x2 = 0;
+		return 1;
+	}
 	if (a == 0)
 	{
 		*x1 = *x2 = -c / b;
